444b9091e1136b8a9c3d94db26c8dd998dd16546
razzaksr <razzaksr@gmail.com>
2023-11-17 14:53:57 +0530

    ListView with function

--- list_statful/lib/main.dart
+++ list_statful/lib/main.dart
@@ -1,12 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:list_statful/Loan.dart';
+import 'package:toast/toast.dart';
 
 void main() {
   runApp(MaterialApp(
     // home:BasicList()
-    home:UsersList()
+    // home:UsersList()
+    home:UserView()
   ));
 }
+
+class UserView extends StatefulWidget {
+  const UserView({super.key});
+
+  @override
+  State<UserView> createState() => _UserViewState();
+}
+
+class _UserViewState extends State<UserView> {
+  List<dynamic> newArraivals=[
+    Loan("Home Loan", "General", 8.4, 5000000, 300),
+    Loan("Gold Loan", "General", 9.2, 2000000, 12),
+    Loan("Personal Loan", "Employees", 16.8, 500000, 36),
+    Loan("Educational Loan", "Students", 10.2, 800000, 64),
+    Loan('Credit Card',"Business",13.7,100000,24)
+  ];
+  @override
+  Widget build(BuildContext context) {
+    ToastContext().init(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('List View Demo'),
+      ),
+      body:ListView.builder(
+          itemCount: newArraivals.length,
+          itemBuilder: (context,index){
+            return Card(
+              child: ListTile(
+                onTap: (){
+                  // print(newArraivals[index]);
+                  Toast.show(newArraivals[index].toString(),duration: Toast.lengthLong);
+                },
+                leading: CircleAvatar(
+                  backgroundColor: Colors.pink,
+                  foregroundColor: Colors.white,
+                  child:Text(newArraivals[index].schemeName[0]),
+                  radius: 50.0,
+                ),
+                title:Text(newArraivals[index].schemeName,
+                  style: TextStyle(fontSize: 14.0,color: Colors.pink),),
+                trailing: OutlinedButton.icon(
+                    onPressed: (){setState(() {
+                      newArraivals.removeAt(index);
+                    });},
+                    icon: Icon(Icons.delete_forever_outlined),
+                    label: Text('Delete'),
+                ),
+              ),
+            );
+          }
+      )
+    );
+  }
+}
+
 
 class UsersList extends StatefulWidget {
   const UsersList({super.key});
